3fc23443703eacb705aef657a738eb96a6d990a2
Safnas <iamsafnas@gmail.com>
2020-04-06 19:15:46 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,10 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const _ = require('lodash');
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -17,18 +14,34 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let posts = [];
+const uri = process.env.ATLAS_URI || 'mongodb://localhost:27017/naazBookDB';
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+
+postSchema = mongoose.Schema({
+  title: String,
+  date: String,
+  content: String,
+  likes: Number
+})
+
+const Post = new mongoose.model("Post", postSchema);
 
 app.get('/', (req, res) => {
-  res.render('home', { startingContent: homeStartingContent, posts: posts })
+  Post.find((err, posts) => {
+    if (err) {
+      console.log('no posts found')
+    } else {
+      res.render('home', { posts: posts })
+    }
+  })
 })
 
 app.get('/about', (req, res) => {
-  res.render('about', { aboutContent: aboutContent })
+  res.render('about')
 })
 
 app.get('/contact', (req, res) => {
-  res.render('contact', { contactContent: contactContent })
+  res.render('contact')
 })
 
 app.get('/compose', (req, res) => {
@@ -36,26 +49,43 @@ app.get('/compose', (req, res) => {
 })
 
 app.post('/compose', (req, res) => {
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postBody
-  }
-  posts.push(post)
+  var today = new Date();
+  const post = new Post({
+    title: _.lowerCase(req.body.postTitle),
+    date: today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear(),
+    content: req.body.postBody,
+    likes: 0
+  })
+  post.save(post)
   res.redirect('/')
 })
 
 app.get("/posts/:postName", (req, res) => {
-
   const requestedTitle = _.lowerCase(req.params.postName);
-  posts.forEach((post) => {
-    const storedTitle = _.lowerCase(post.title);
-    if (storedTitle === requestedTitle) {
-      res.render('post', { post: post })
+  Post.findOne({ title: requestedTitle }, (err, post) => {
+    if (err) {
+      console.log('no post found with requested title')
+    } else {
+      if (post !== null) {
+        res.render('post', { post: post })
+      } else {
+        res.redirect('/')
+      }
     }
   })
-
-
 })
+
+app.post('/like', (req, res) => {
+  const title = req.body.title;
+  const likes = Number.parseInt(req.body.likes) + 1;
+  Post.findOneAndUpdate({ title: title }, { $set: { likes: likes } }, (err) => {
+    if (!err) {
+      console.log('updated successfully')
+    }
+  })
+  res.redirect('/posts/' + title)
+})
+
 
 app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000");
